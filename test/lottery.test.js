b478122966d78be1eea2a4138b3a3a3dc42e4de5
Olubo Ifeoluwa <ifeoluwaolubo@gmail.com>
2023-09-30 17:26:58 +0100

    tested that the right amount of ether is sent before entering the lottery

--- test/lottery.test.js
+++ test/lottery.test.js
@@ -31,4 +31,34 @@ describe("Lottery Contract", () => {
     assert.equal(accounts[1], players[0]);
     assert.equal(1, players.length);
   });
+
+  it("allows multiple accounts to enter", async () => {
+    await lottery.methods
+      .enter()
+      .send({ from: accounts[1], value: web3.utils.toWei("0.02", "ether") });
+    await lottery.methods
+      .enter()
+      .send({ from: accounts[2], value: web3.utils.toWei("0.015", "ether") });
+    await lottery.methods
+      .enter()
+      .send({ from: accounts[3], value: web3.utils.toWei("0.022", "ether") });
+
+    const players = await lottery.methods
+      .getPlayers()
+      .call({ from: accounts[1] });
+
+    assert.equal(accounts[1], players[0]);
+    assert.equal(accounts[2], players[1]);
+    assert.equal(accounts[3], players[2]);
+    assert.equal(3, players.length);
+  });
+
+  it("requires a minimum amount of ether to enter the lottery", async () => {
+    try {
+      await lottery.methods.enter().send({ from: accounts[1], value: "200" });
+      assert(false);
+    } catch (error) {
+      assert(error);
+    }
+  });
 });
