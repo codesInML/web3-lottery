012426b1976538af3d52731586552b73ff39940d
Olubo Ifeoluwa <ifeoluwaolubo@gmail.com>
2023-09-30 17:13:08 +0100

    wrote test for a player to enter the lottery

--- test/lottery.test.js
+++ test/lottery.test.js
@@ -18,4 +18,17 @@ describe("Lottery Contract", () => {
   it("deploys a contract", () => {
     assert.ok(lottery.options.address);
   });
+
+  it("allows an account to enter", async () => {
+    await lottery.methods
+      .enter()
+      .send({ from: accounts[1], value: web3.utils.toWei("0.02", "ether") });
+
+    const players = await lottery.methods
+      .getPlayers()
+      .call({ from: accounts[1] });
+
+    assert.equal(accounts[1], players[0]);
+    assert.equal(1, players.length);
+  });
 });
